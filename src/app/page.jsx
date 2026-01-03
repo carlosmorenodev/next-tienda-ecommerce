1ab24d47d4c1438989f912eb5bcbae30b969dfbe
Carlos Moreno <carlosmorenomartindev@gmail.com>
2026-01-03 18:57:47 +0100

    Añadidos estilos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,28 +13,28 @@ export default function Home() {
             key={product.id}
             className={styles.productCard}
           >
-            <div className={styles.productCardLabels}>
-              <ul>
-                {product.etiquetas.map((etiqueta, index) => (
-                  // Es importante incluir una 'key' única cuando se usa map
-                  // para crear una lista de elementos en React. Usar el 'index'
-                  // es aceptable si la lista es estática y no cambia de orden.
-                  <li key={index}>
-                    {etiqueta}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <h2 className={styles.productCardTitle}>{product.nombre}</h2>
             <Image
               src={product.imagen}
               alt={product.nombre}
               width={300}
               height={300}
             />
+            <h2 className={styles.productCardTitle}>{product.nombre}</h2>
+
+
             <p className={styles.productCardPrice}>{product.precio} €</p>
             <p className={styles.productCardDescription}>{product.descripcion}</p>
 
+            <div className={styles.productCardLabels}>
+              <ul>
+                {product.etiquetas.map((etiqueta, index) => (
+                  <li key={index}>
+                    {etiqueta}
+                  </li>
+                ))}
+              </ul>
+            </div>
+           
           </Link>
         ))}
       </main>
